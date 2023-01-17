818724aa3cf5b47f3d1d4907c31f4636a01a628a
Imtiaj Ahammad <imtiaj.ahammad@selise.ch>
2023-01-17 16:25:28 +0600

    Identity context, User added

---
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,29 @@
+using Infrastructure.Constants;
+using Infrastructure.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity;
+
+public class AppIdentityDbContextSeed
+{
+    public static async Task SeedAsync(AppIdentityDbContext identityDbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+
+        if (identityDbContext.Database.IsSqlServer())
+        {
+            identityDbContext.Database.Migrate();
+        }
+
+        await roleManager.CreateAsync(new IdentityRole(Authorization.Constants.Roles.ADMINISTRATORS));
+
+        var defaultUser = new ApplicationUser { UserName = "demouser@microsoft.com", Email = "demouser@microsoft.com" };
+        await userManager.CreateAsync(defaultUser, AuthorizationConstants.DEFAULT_PASSWORD);
+
+        string adminUserName = "admin@microsoft.com";
+        var adminUser = new ApplicationUser { UserName = adminUserName, Email = adminUserName };
+        await userManager.CreateAsync(adminUser, AuthorizationConstants.DEFAULT_PASSWORD);
+        adminUser = await userManager.FindByNameAsync(adminUserName);
+        await userManager.AddToRoleAsync(adminUser, Authorization.Constants.Roles.ADMINISTRATORS);
+    }
+}
---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
