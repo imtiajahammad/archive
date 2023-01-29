ac457f129ac8b69f34f0982a648ab3f70d8181f3
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-01-29 22:54:46 +0600

    setting up db context

---
+++ src/Infrastructure/Dependencies.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace infrastructure
+{
+	public static class Dependencies
+	{
+		public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+		{
+			/*var useOnlyInMemoryDatabase = false;
+			if (configuration["UseOnlyInMemoryDatabase"] != null)
+			{
+				useOnlyInMemoryDatabase = bool.Parse(configuration["UseOnlyInMemoryDatabase"]);
+			}
+			if (useOnlyInMemoryDatabase)
+			{
+				services.AddDbContext<ArchiveContext>(c => c.UseInMemoryDatabase("Catalog"));
+				services.AddDbContext<AppIdentityDbContext>(options => options.UseInMemoryDatabase("Identity"));
+			}
+			else
+			{*/
+				// use real database
+				// requires LocalDB which can be installed with SQL Server Express 2016
+				services.AddDbContext<ArchiveContext>(c => c.UseSqlServer(configuration.GetConnectionString("ArchiveConnection")));
+				services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("IdentityConnection")));
+			//}
+		}
+	}
+}
+
+/*
+-- go to publicApi project and run these commands
+
+dotnet restore
+dotnet tool restore
+
+dotnet ef migrations add InitialModel --context archiveContext -p ../Infrastructure/Infrastructure.csproj -s PublicApi.csproj -o Data/Migrations
+
+dotnet ef migrations add InitialIdentityModel --context appIdentityDbContext -p ../Infrastructure/Infrastructure.csproj -s PublicApi.csproj -o Identity/Migrations
+
+
+dotnet ef database update -c archiveContext -p ../Infrastructure/Infrastructure.csproj -s PublicApi.csproj
+dotnet ef database update -c appIdentityDbContext -p ../Infrastructure/Infrastructure.csproj -s PublicApi.csproj
+*/
--- src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -17,10 +17,10 @@ public class AppIdentityDbContextSeed
 
         await roleManager.CreateAsync(new IdentityRole(Constants.Roles.ADMINISTRATORS));
 
-        var defaultUser = new ApplicationUser { UserName = "demouser@microsoft.com", Email = "demouser@microsoft.com" };
+        var defaultUser = new ApplicationUser { UserName = "demouser@archive.com", Email = "demouser@archive.com" };
         await userManager.CreateAsync(defaultUser, AuthorizationConstants.DEFAULT_PASSWORD);
 
-        string adminUserName = "admin@microsoft.com";
+        string adminUserName = "admin@archive.com";
         var adminUser = new ApplicationUser { UserName = adminUserName, Email = adminUserName };
         await userManager.CreateAsync(adminUser, AuthorizationConstants.DEFAULT_PASSWORD);
         adminUser = await userManager.FindByNameAsync(adminUserName);
