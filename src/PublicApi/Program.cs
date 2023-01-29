47750e260b14b42cd07e80e8c7da2b634d831416
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-01-30 00:33:07 +0600

    application seeding setup on program

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -1,8 +1,17 @@
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Infrastructure.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<AppIdentityDbContext>()
+        .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,6 +19,32 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+/* Application Seeding started */
+
+app.Logger.LogInformation("PublicApi App created...");
+
+app.Logger.LogInformation("Seeding Database...");
+
+using (var scope = app.Services.CreateScope())
+{
+    var scopedProvider = scope.ServiceProvider;
+    try
+    {
+        var archiveContext = scopedProvider.GetRequiredService<ArchiveContext>();
+        await ArchiveContextSeed.SeedAsync(archiveContext, app.Logger);
+
+        var userManager = scopedProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scopedProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var identityContext = scopedProvider.GetRequiredService<AppIdentityDbContext>();
+        await AppIdentityDbContextSeed.SeedAsync(identityContext, userManager, roleManager);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
+/* Application Seeding ended */
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
